2af982ad9d36bbf4c77794de13c7481feafd884d
HaekyungSeong <haekyungs95@gmail.com>
2023-06-01 12:26:14 +0900

    하단 고정바 제작완료

--- jam-project/BottonMenu.js
+++ jam-project/BottonMenu.js
@@ -1,55 +1,56 @@
 import React from 'react';
 import { View, Text, TouchableOpacity, Image } from 'react-native';
 import { Styles, StylesText } from './Styles.js';
+import { navigation } from '@react-navigation/native';
 
-export default function BottomMenu() {
+export default function BottomMenu({ navigation }) {
   return (
-    <View style={Styles.bottonBox}>
-      <View style={{ height: '100%', width: '20%', display: 'flex', alignItems: 'center' }}>
+    <View style={Styles.bottomBox}>
+      <View style={Styles.bottomView}>
         {/* 버튼은 이미지를 넣을 수 없어서 사진을 포함한 버튼기능인 TouchableOpacity을 사용. */}
-        <TouchableOpacity style={{ height: '70%', width: '80%' }}>
+        <TouchableOpacity style={Styles.bottomTouch} onPress={() => navigation.navigate('')}>
           <Image
             source={require('./resource/realtime.png')}
-            style={{ height: '100%', width: '100%' }}
+            style={Styles.bottomImg}
           />
+          <Text style={Styles.bottomText}>실시간</Text>
         </TouchableOpacity>
-        <Text>실시간</Text>
       </View>
-      <View style={{ height: '100%', width: '20%', display: 'flex', alignItems: 'center' }}>
-        <TouchableOpacity style={{ height: '70%', width: '80%' }}>
+      <View style={Styles.bottomView}>
+        <TouchableOpacity style={Styles.bottomTouch} onPress={() => navigation.navigate('')}>
           <Image
             source={require('./resource/recommand.png')}
-            style={{ height: '100%', width: '100%' }}
+            style={Styles.bottomImg}
           />
+          <Text style={Styles.bottomText}>주식 추천</Text>
         </TouchableOpacity>
-        <Text>주식 추천</Text>
       </View>
-      <View style={{ height: '100%', width: '20%', display: 'flex', alignItems: 'center' }}>
-        <TouchableOpacity style={{ height: '70%', width: '80%' }}>
+      <View style={Styles.bottomView}>
+        <TouchableOpacity style={Styles.bottomTouch} onPress={() => navigation.navigate('')}>
           <Image
             source={require('./resource/search.png')}
-            style={{ height: '100%', width: '100%' }}
+            style={Styles.bottomImg}
           />
+          <Text style={Styles.bottomText}>검색</Text>
         </TouchableOpacity>
-        <Text>검색</Text>
       </View>
-      <View style={{ height: '100%', width: '20%', display: 'flex', alignItems: 'center' }}>
-        <TouchableOpacity style={{ height: '70%', width: '80%' }}>
+      <View style={Styles.bottomView}>
+        <TouchableOpacity style={Styles.bottomTouch} onPress={() => navigation.navigate('')}>
           <Image
             source={require('./resource/guide.png')}
-            style={{ height: '100%', width: '100%' }}
+            style={Styles.bottomImg}
           />
+          <Text style={Styles.bottomText}>가이드</Text>
         </TouchableOpacity>
-        <Text>가이드</Text>
       </View>
-      <View style={{ height: '100%', width: '20%', display: 'flex', alignItems: 'center' }}>
-        <TouchableOpacity style={{ height: '60%', width: '80%' }}>
+      <View style={Styles.bottomView}>
+        <TouchableOpacity style={Styles.bottomTouch} onPress={() => navigation.navigate('')}>
           <Image
             source={require('./resource/wallet.png')}
-            style={{ height: '100%', width: '100%' }}
+            style={Styles.bottomImg}
           />
+          <Text style={Styles.bottomText}>지갑</Text>
         </TouchableOpacity>
-        <Text>지갑</Text>
       </View>
     </View>
   );
