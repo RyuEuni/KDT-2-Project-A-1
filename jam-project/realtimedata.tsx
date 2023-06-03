61e3ba670ec57235323cc29024524c7782dc66c1
HaekyungSeong <haekyungs95@gmail.com>
2023-06-03 17:20:30 +0900

    실시간 페이지의 기업들 정보

---
+++ jam-project/realtimedata.tsx
@@ -0,0 +1,122 @@
+export const realtimedata = [
+  {
+    logo: '',
+    companyname: '삼성전자',
+    price: 100
+  },
+  {
+    logo: '',
+    companyname: 'CJ',
+    price: 200
+  },
+  {
+    logo: '',
+    companyname: '동아제약',
+    price: 0
+  },
+  {
+    logo: '',
+    companyname: '삼성전자',
+    price: 100
+  },
+  {
+    logo: '',
+    companyname: 'CJ',
+    price: 200
+  },
+  {
+    logo: '',
+    companyname: '동아제약',
+    price: 0
+  },
+  {
+    logo: '',
+    companyname: '삼성전자',
+    price: 100
+  },
+  {
+    logo: '',
+    companyname: 'CJ',
+    price: 200
+  },
+  {
+    logo: '',
+    companyname: '동아제약',
+    price: 0
+  },
+  {
+    logo: '',
+    companyname: '삼성전자',
+    price: 100
+  },
+  {
+    logo: '',
+    companyname: 'CJ',
+    price: 200
+  },
+  {
+    logo: '',
+    companyname: '동아제약',
+    price: 0
+  },
+  {
+    logo: '',
+    companyname: '삼성전자',
+    price: 100
+  },
+  {
+    logo: '',
+    companyname: 'CJ',
+    price: 200
+  },
+  {
+    logo: '',
+    companyname: '동아제약',
+    price: 0
+  },
+  {
+    logo: '',
+    companyname: '삼성전자',
+    price: 100
+  },
+  {
+    logo: '',
+    companyname: 'CJ',
+    price: 200
+  },
+  {
+    logo: '',
+    companyname: '동아제약',
+    price: 0
+  },
+  {
+    logo: '',
+    companyname: '삼성전자',
+    price: 100
+  },
+  {
+    logo: '',
+    companyname: 'CJ',
+    price: 200
+  },
+  {
+    logo: '',
+    companyname: '동아제약',
+    price: 0
+  },
+  {
+    logo: '',
+    companyname: '삼성전자',
+    price: 100
+  },
+  {
+    logo: '',
+    companyname: 'CJ',
+    price: 200
+  },
+  {
+    logo: '',
+    companyname: '동아제약',
+    price: 0
+  },
+]
