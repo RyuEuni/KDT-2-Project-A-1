fb9c47e3b70adba758fa57d6ac236086e04b94d3
HaekyungSeong <haekyungs95@gmail.com>
2023-06-06 17:49:39 +0900

    로그인 로직 완료~ 저는 천잰가봐요 은이님:)

--- jam-project/Utils/DB/server.ts
+++ jam-project/Utils/DB/server.ts
@@ -3,33 +3,41 @@ import { DBInfo } from './DBConnect';
 
 const app = express();
 
-DBInfo.connect()
-
-interface Body {
-  id: string
-  pw: string
-}
+DBInfo.connect(()=>{
+  console.log('Database open! port is 3306')
+})
+// DB 연결
 
 
-app.post('/database', (req: Request, res: Response) => {
+app.post('/login', (req: Request, res: Response) => {
   // DB에서 데이터 조회 등의 로직 수행
 
-  let body: Body;
+  let body = "";
 
   req.on("data", (data) => {
     body += data;
   });
+
   req.on("end", () => {
-    console.log("확인중" + body)
+    // console.log("확인중" + body)
 
-    DBInfo.query(`select * from testID where ID=${body.id}`, (error, result) => {
-      console.log(result)
+    // console.log(typeof body)
+    // console.log(JSON.parse(body))
+    // console.log(JSON.parse(body).id)
 
+    // 넘겨받은 아이디와 비밀번호를 변수에 담기
+    let loginDataid = JSON.parse(body).id
+    let loginDatapw = JSON.parse(body).pw
+
+    // console.log(loginDataid)
+
+    // 쿼리문으로 해당 아이디가 유저 목록에 있는지 확인
+    DBInfo.query(`select * from testID where ID='${loginDataid}'`, (error, result) => {
+      // console.log(result)
       
-
+      // 결과값을 전달.
+      res.json(result)
     })
-
-    // res.json(JSON.parse(body))
   });
 });
 
--- jam-project/View/page/login.tsx
+++ jam-project/View/page/login.tsx
@@ -1,5 +1,5 @@
 import React, { useState } from 'react';
-import { View, Text, Button, Image, TextInput, TouchableOpacity } from 'react-native';
+import { View, Text, Button, Image, TextInput, TouchableOpacity, Alert } from 'react-native';
 import { Styles, StylesText } from '../style/styles';
 import Icon from 'react-native-vector-icons/AntDesign'
 import { response } from 'express';
@@ -7,30 +7,63 @@ import { response } from 'express';
 
 
 const LoginScreen: React.FC<any> = ({ navigation }) => {
+
   const [loginText, setLoginText] = useState('');
   const [passwordText, setPasswordText] = useState('');
 
   const loginResult = () => {
-    console.log("id: ", loginText, " pw: ", passwordText);
+    // console.log("id: ", loginText, " pw: ", passwordText);
+
+    // 아이디 비밀번호를 변수에 담는다.
     const data = {
       id: loginText,
       pw: passwordText
     }
-    fetch('http://121.184.34.150:3080/database', {
+
+    // 패치 시 주소는 실행하는 컴터 ip
+    fetch('http://121.184.34.150:3080/login', {
       method: 'post',
       headers: {
         'Content-Type': 'application/json',
       },
+
+      // 입력받은 데이터를 바디에 담아서 전송
       body: JSON.stringify(data)
     }
     ).then((response) => {
+
+      // 서버로 부터 응답을 회신받는다.
       return response.json()
     })
-      .then(json => console.log("테스트중" + JSON.stringify(json))
+      .then(json => {
+
+
+        // console.log("테스트중" + JSON.stringify(json))
+        // console.log(typeof data)
+        
+        // 데이터 변수에 json화 한 값을 담는다.
+        const data = JSON.stringify(json)
+        
+        // console.log(data.split(`"`)[3])
+
+        if (data.split(`"`)[3] === undefined) {
+          // 아이디가 없음
+          Alert.alert('아이디 실패', '아이디가 일치하지 않습니다.')
+        } else if (data.split(`"`)[7] !== passwordText) {
+          // 비밀번호 틀림
+          Alert.alert('비밀번호 실패', '비밀번호가 일치하지 않습니다.')
+        } else {
+          // 아이디 비번 있으니까 입장~
+          // 존맛탱 주기 
+          Alert.alert('환영합니다!','잼픽이와 함께 하는 JamStock에 오신 것을 환영합니다!')
+          navigation.navigate('home')
+        }
+      }
       ).catch(() => {
         console.log('error')
       })
   };
+
 
   return (
     <View style={Styles.loginBox}>
@@ -72,7 +105,6 @@ const LoginScreen: React.FC<any> = ({ navigation }) => {
       </View>
       <TouchableOpacity style={Styles.loginPageLoginBtn} onPress={() => {
         loginResult()
-        navigation.navigate('home')
       }
       }>
         <Text style={{ fontSize: StylesText.sizeMedium.fontSize, textAlign: 'center', marginTop: 7 }}>로그인</Text>
