a673d9e7b5776a2e76f2dcd5598fe5e6c79a0739
HaekyungSeong <haekyungs95@gmail.com>
2023-06-12 15:51:48 +0900

    회원가입에 최초로그인 구분을 위한 셀 추가

--- jam-project/Utils/DB/signUp.ts
+++ jam-project/Utils/DB/signUp.ts
@@ -38,7 +38,6 @@ export function signCheck(request: Request, response: Response) {
       })
 
   });
-  // res.json(data);
 
 }
 
@@ -58,11 +57,10 @@ export function signResult(request: Request, response: Response) {
       bodySplit.push(bodycarrier[i]);
     }
 
-    DBInfo.query(`INSERT INTO userinfo(ID,password,nickname,email,birthday) values ('${bodySplit[0]}', '${bodySplit[1]}', '${bodySplit[2]}', '${bodySplit[4]}', '${bodySplit[3]}')`)
+    DBInfo.query(`INSERT INTO userinfo(ID,password,nickname,email,birthday,firstentrance) values ('${bodySplit[0]}', '${bodySplit[1]}', '${bodySplit[2]}', '${bodySplit[4]}', '${bodySplit[3]}',0)`)
     response.writeHead(200);
     response.end();
 
-    // conn.end();
 
   });
 
