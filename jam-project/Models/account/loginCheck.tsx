5667e386b123155226c79789244cea745273aa8b
HaekyungSeong <haekyungs95@gmail.com>
2023-06-13 13:08:03 +0900

    로그인 결과를 판별하는 함수 제작 및 반영+비밀번호가리기 추가

---
+++ jam-project/Models/account/loginCheck.tsx
@@ -0,0 +1,48 @@
+import { Alert } from "react-native";
+
+// logindata 에 대한 타입 지정
+interface logindata{
+  id:string
+  password:string
+}
+
+// 로그인 결과에 대한 함수
+export const loginResult = (data:logindata, navigation:any) => {
+
+    // 패치 시 주소는 실행하는 컴터 ip
+    fetch('http://192.168.30.76:3080/login', {
+      method: 'post',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+
+      // 입력받은 데이터를 바디에 담아서 전송
+      body: JSON.stringify(data)
+    }
+    ).then((response) => {
+
+      // 서버로 부터 응답을 회신받는다.
+      return response.json()
+    })
+      .then(json => {
+        
+        // 데이터 변수에 json화 한 값을 담는다.
+        const datas = JSON.stringify(json)
+
+        if (datas.split(`"`)[5] === undefined) {
+          // 아이디가 없음
+          Alert.alert('아이디 실패', '아이디가 일치하지 않습니다.')
+        } else if (datas.split(`"`)[9] !== data.password) {
+          // 비밀번호 틀림
+          Alert.alert('비밀번호 실패', '비밀번호가 일치하지 않습니다.')
+        } else {
+          // 아이디 비번 있으니까 입장~
+          
+          Alert.alert('환영합니다!','잼픽이와 함께 하는 JamStock에 오신 것을 환영합니다!')
+          navigation.navigate('home')
+        }
+      }
+      ).catch(() => {
+        console.log('error')
+      })
+  };
--- jam-project/View/page/login.tsx
+++ jam-project/View/page/login.tsx
@@ -1,72 +1,26 @@
 import React, { useState } from 'react';
-import { View, Text, Button, Image, TextInput, TouchableOpacity, Alert } from 'react-native';
+import { View, Text, Image, TextInput, TouchableOpacity} from 'react-native';
 import { Styles, StylesText } from '../style/styles';
 import Icon from 'react-native-vector-icons/AntDesign'
-import { response } from 'express';
+import { loginResult } from '../../Models/account/loginCheck';
 
 
 
 const LoginScreen: React.FC<any> = ({ navigation }) => {
 
-  const [loginText, setLoginText] = useState('');
+  // 아이디, 비밀번호 입력값
+  const [idText, setIdText] = useState('');
   const [passwordText, setPasswordText] = useState('');
 
-  const loginResult = () => {
-    // console.log("id: ", loginText, " pw: ", passwordText);
-
-    // 아이디 비밀번호를 변수에 담는다.
-    const data = {
-      id: loginText,
-      pw: passwordText
-    }
-
-    // 패치 시 주소는 실행하는 컴터 ip
-    fetch('http://121.184.34.150:3080/login', {
-      method: 'post',
-      headers: {
-        'Content-Type': 'application/json',
-      },
-
-      // 입력받은 데이터를 바디에 담아서 전송
-      body: JSON.stringify(data)
-    }
-    ).then((response) => {
-
-      // 서버로 부터 응답을 회신받는다.
-      return response.json()
-    })
-      .then(json => {
-
-
-        // console.log("테스트중" + JSON.stringify(json))
-        // console.log(typeof data)
-        
-        // 데이터 변수에 json화 한 값을 담는다.
-        const data = JSON.stringify(json)
-        
-        // console.log(data.split(`"`)[3])
-
-        if (data.split(`"`)[3] === undefined) {
-          // 아이디가 없음
-          Alert.alert('아이디 실패', '아이디가 일치하지 않습니다.')
-        } else if (data.split(`"`)[7] !== passwordText) {
-          // 비밀번호 틀림
-          Alert.alert('비밀번호 실패', '비밀번호가 일치하지 않습니다.')
-        } else {
-          // 아이디 비번 있으니까 입장~
-          // 존맛탱 주기 
-          Alert.alert('환영합니다!','잼픽이와 함께 하는 JamStock에 오신 것을 환영합니다!')
-          navigation.navigate('home')
-        }
-      }
-      ).catch(() => {
-        console.log('error')
-      })
-  };
-
-
+  // 아이디 비밀번호를 변수에 담은 data라는 객체.
+  const data = {
+    id: idText,
+    password: passwordText
+  }
+  
   return (
     <View style={Styles.loginBox}>
+      
       {/* 뒤로가기 */}
       <TouchableOpacity style={Styles.backButton} onPress={() => navigation.pop()}>
         <Icon name="left" size={25} color='#A5C7FB' style={{ marginTop: '50%' }} />
@@ -79,8 +33,8 @@ const LoginScreen: React.FC<any> = ({ navigation }) => {
         <Text style={Styles.idPwText}>ID</Text>
         <TextInput
           style={Styles.idPwInput}
-          onChangeText={text => setLoginText(text)}
-          value={loginText}
+          onChangeText={text => setIdText(text)}
+          value={idText}
           placeholder="ID를 입력해 주세요"
         />
       </View>
@@ -89,8 +43,9 @@ const LoginScreen: React.FC<any> = ({ navigation }) => {
         <TextInput
           style={Styles.idPwInput}
           onChangeText={text => setPasswordText(text)}
+          secureTextEntry={true}
           value={passwordText}
-          placeholder="PW를 입력해 주세요"
+          placeholder="비밀번호를 입력해 주세요"
         />
       </View>
 
@@ -104,7 +59,9 @@ const LoginScreen: React.FC<any> = ({ navigation }) => {
         </TouchableOpacity>
       </View>
       <TouchableOpacity style={Styles.loginPageLoginBtn} onPress={() => {
-        loginResult()
+        loginResult(data, navigation)
+        setIdText('')
+        setPasswordText('')
       }
       }>
         <Text style={{ fontSize: StylesText.sizeMedium.fontSize, textAlign: 'center', marginTop: 7 }}>로그인</Text>
