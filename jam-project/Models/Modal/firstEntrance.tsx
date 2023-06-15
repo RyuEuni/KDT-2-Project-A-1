0d8ab19584ef2a57bca233d0957597901d9c35cc
HaekyungSeong <haekyungs95@gmail.com>
2023-06-15 22:27:43 +0900

    폴더 재구분

--- jam-project/Models/Modal/firstEntrance.tsx
+++ jam-project/Models/Modal/firstEntrance.tsx
@@ -3,20 +3,23 @@ import { Modal, View, Image, Text,TouchableOpacity } from "react-native";
 import { Styles } from "../../View/style/styles";
 import { response } from "express";
 import  Icon  from "react-native-vector-icons/AntDesign";
+import url from '../func/fetchURL'
+import {callUser} from '../../Utils/Storage/callID'
 
-fetch('/entrance', {
+let [entrance, setEntrance] = useState(false)
+
+fetch(`${url}/entrance`, {
   method: 'POST',
   headers: {
     'Content-Type': 'aplication/json'
   },
-  body: ''
+  body: JSON.stringify(`${callUser()[0]}`)
 })
   .then(response => response.json())
   .then(data => {
-
+    console.log("온게없을텐데",data)
   }).catch(err => console.error('warning!' + err))
 
-let [entrance, setEntrance] = useState(false)
 
 const Entrance = () => {
   <View style={Styles.entranceBox}>
